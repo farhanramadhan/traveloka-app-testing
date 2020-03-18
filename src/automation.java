3b0abd9aab7ad5399b801a0f7686ed794593fa8a
Farhan Ramadhan <farhanramadhansk@gmail.com>
2020-03-18 23:13:02 +0700

    add assertion after choose from and to city and chosen date

--- src/automation.java
+++ src/automation.java
@@ -13,7 +13,13 @@ import java.net.URL;
 import java.util.List;
 
 public class automation {
+
+    public static final int TIME_LIMIT = 2;
+
     public static void main(String args[]) throws MalformedURLException {
+
+        String fromDestination = "Jakarta (JKTA)";
+        String toDestination = "Surabaya (SUB)";
 
         DesiredCapabilities dc = new DesiredCapabilities();
         setCapability(dc);
@@ -31,39 +37,54 @@ public class automation {
 //        MobileElement el1 = (MobileElement) driver.findElementByXPath("/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.FrameLayout/android.widget.FrameLayout/android.view.View/android.widget.FrameLayout/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.RelativeLayout/android.widget.ScrollView/android.widget.LinearLayout/android.widget.LinearLayout[1]/android.widget.RelativeLayout/android.widget.RelativeLayout");
 //        el1.click();
 
-        while (!(elementExist(driver, 3, "com.traveloka.android:id/image_view_product_icon")));
+        while (!(elementExist(driver, TIME_LIMIT, "com.traveloka.android:id/image_view_product_icon")));
         MobileElement FlightsButton = (MobileElement) driver.findElement(By.xpath ("//android.widget.ImageView[@bounds='[42,489][174,621]']"));
         FlightsButton.click();
 
-        while (!(elementExist(driver, 3, "com.traveloka.android:id/widgetView")));
+        while (!(elementExist(driver, TIME_LIMIT, "com.traveloka.android:id/widgetView")));
         MobileElement FromButton = (MobileElement) driver.findElement(By.xpath ("//android.widget.EditText[@bounds='[84,516][996,636]']"));
         FromButton.click();
 
-        while (!(elementExist(driver, 3, "com.traveloka.android:id/text_view_geo_name")));
+        while (!(elementExist(driver, TIME_LIMIT, "com.traveloka.android:id/text_view_geo_name")));
         MobileElement JakartaChoiceButton = (MobileElement) driver.findElement(By.xpath ("//android.widget.TextView[@bounds='[48,398][446,463]']"));
         JakartaChoiceButton.click();
 
-        while (!(elementExist(driver, 3, "com.traveloka.android:id/widgetView")));
+        while (!(elementExist(driver, TIME_LIMIT, "com.traveloka.android:id/widgetView")));
+
+        // Assert From City
+        Assert.assertEquals(FromButton.getText(), fromDestination);
+
         MobileElement ToButton = (MobileElement) driver.findElement(By.xpath ("//android.widget.EditText[@bounds='[84,756][996,876]']"));
         ToButton.click();
 
-        while (!(elementExist(driver, 3, "com.traveloka.android:id/text_view_geo_name")));
+        while (!(elementExist(driver, TIME_LIMIT, "com.traveloka.android:id/text_view_geo_name")));
         MobileElement SurabayaChoiceButton = (MobileElement) driver.findElement(By.xpath ("//android.widget.TextView[@bounds='[48,557][482,622]']"));
         SurabayaChoiceButton.click();
 
-        while (!(elementExist(driver, 3, "com.traveloka.android:id/widgetView")));
+        while (!(elementExist(driver, TIME_LIMIT, "com.traveloka.android:id/widgetView")));
+
+        // Assert To Destination City
+        Assert.assertEquals(ToButton.getText(), toDestination);
+
         MobileElement DepartureDateButton = (MobileElement) driver.findElement(By.xpath ("//android.widget.EditText[@bounds='[84,996][996,1116]']"));
         DepartureDateButton.click();
 
-        while (!(elementExist(driver, 3, "com.traveloka.android:id/info_top_text_view")));
+        while (!(elementExist(driver, TIME_LIMIT, "com.traveloka.android:id/info_top_text_view")));
         List<AndroidElement> elements = driver.findElements(By.xpath ("//android.widget.TextView[@bounds='[503,796][577,840]']/../*"));
         int indexDate = Integer.parseInt(elements.get(1).getAttribute("text"));
+        indexDate += 1;
+        String choosenIndexDate = Integer.toString(indexDate);
         indexDate += 1;
         String choosenDate = Integer.toString(indexDate);
 
 //        while (!(elementExist(driver, 3, "com.traveloka.android:id/widgetView")));
-        MobileElement theDayAfterTommorowDate = (MobileElement) driver.findElement(By.xpath ("//android.widget.RelativeLayout[@index='" + choosenDate + "']"));
+        MobileElement theDayAfterTommorowDate = (MobileElement) driver.findElement(By.xpath ("//android.widget.RelativeLayout[@index='" + choosenIndexDate + "']"));
         theDayAfterTommorowDate.click();
+
+        while (!(elementExist(driver, TIME_LIMIT, "com.traveloka.android:id/widgetView")));
+
+        // Assert Chosen Date
+        Assert.assertTrue(DepartureDateButton.getText().contains(choosenDate));
 //        MobileElement el10 = (MobileElement) driver.findElementByXPath("5ad3d6c6-330f-487e-bce0-e14269aee459");
 //        el10.click();
 
@@ -94,19 +115,19 @@ public class automation {
     }
 
     public static void InitialChooseLanguage(AndroidDriver driver) {
-        while (!(elementExist(driver, 5, "com.traveloka.android:id/text_continue")));
+        while (!(elementExist(driver, TIME_LIMIT, "com.traveloka.android:id/text_continue")));
         MobileElement el2 = (MobileElement) driver.findElementById("com.traveloka.android:id/text_continue");
         el2.click();
 
-        while (!(elementExist(driver, 5, "com.traveloka.android:id/text_skip")));
+        while (!(elementExist(driver, TIME_LIMIT, "com.traveloka.android:id/text_skip")));
         MobileElement el3 = (MobileElement) driver.findElementById("com.traveloka.android:id/text_skip");
         el3.click();
 
-        while (!(elementExist(driver, 5, "com.traveloka.android:id/text_skip")));
+        while (!(elementExist(driver, TIME_LIMIT, "com.traveloka.android:id/text_skip")));
         MobileElement el4 = (MobileElement) driver.findElementById("com.traveloka.android:id/text_skip");
         el4.click();
 
-        while (!(elementExist(driver, 5, "com.traveloka.android:id/text_view_tooltip_ok")));
+        while (!(elementExist(driver, TIME_LIMIT, "com.traveloka.android:id/text_view_tooltip_ok")));
         MobileElement el6 = (MobileElement) driver.findElementById("com.traveloka.android:id/text_view_tooltip_ok");
         el6.click();
     }
