fe04f510428330aa0ec5f316748e100660a91eeb
Farhan Ramadhan <farhanramadhansk@gmail.com>
2020-03-20 04:58:43 +0700

    refactor code

--- src/automation.java
+++ src/automation.java
@@ -6,6 +6,7 @@ import io.appium.java_client.remote.MobileCapabilityType;
 import io.appium.java_client.touch.TapOptions;
 import io.appium.java_client.touch.WaitOptions;
 import io.appium.java_client.touch.offset.PointOption;
+import org.apache.commons.lang3.reflect.TypeUtilsTest;
 import org.openqa.selenium.By;
 import org.openqa.selenium.remote.DesiredCapabilities;
 import org.openqa.selenium.support.ui.ExpectedConditions;
@@ -29,30 +30,93 @@ public class automation {
 
         final String NameContactDetails = "Jidapa Ornec";
         final String MobileNumberContactDetails = "81234314123";
-        final String EmailContactDetails = "JidapaOrc1241321@gmail.com";
+        final String EmailContactDetails = "JidapaOrc3241321@gmail.com";
 
         final String AdultName = "Jimmy Hendrix";
 
         final String ChildName = "adelyn rihana";
-        final String ChildBirth = "2 March 2010";
         final String ChildDate = "2";
         final String ChildMonth = "Mar";
         final String ChildYear = "2010";
 
         final String InfantName = "ray marco";
-        final String InfantBirth = "10 April 2019";
         final String InfantDate = "10";
         final String InfantMonth = "Apr";
         final String InfantYear = "2019";
 
+        // Setup Appium
         DesiredCapabilities dc = new DesiredCapabilities();
         setCapability(dc);
 
+        // Setup Android
         AndroidDriver<AndroidElement> driver = new AndroidDriver<AndroidElement>(new URL("http://127.0.0.1:4723/wd/hub"), dc);
 
-
+        // Testing The App
         InitialChooseLanguage(driver);
+        AssertOriginCity(driver, fromDestination);
+        AssertDestinationCity(driver, toDestination);
+        AssertDate(driver);
+        AssertPassenger(driver);
+        AssertFlightClass(driver);
+        AssertChooseFlightSchedule(driver);
+        AssertContactDetails(driver, NameContactDetails, MobileNumberContactDetails, EmailContactDetails);
+        AssertAdult(driver, AdultName);
+        AssertChild(driver, ChildName, ChildDate, ChildMonth, ChildYear);
+        AssertInfant(driver, InfantName, InfantDate, InfantMonth, InfantYear);
+        AssertFlightDetail(driver);
+    }
 
+    public static void swipe(AndroidDriver driver ,int startX, int endX, int startY, int endY) {
+        try {
+            new TouchAction(driver).press(PointOption.point(startX, startY)).waitAction(WaitOptions.waitOptions(Duration.ofSeconds(1)))
+                    .moveTo(PointOption.point(endX, endY)).release().perform();
+        } catch (Exception e) {
+            System.out.println("unable to swipe");
+        }
+    }
+
+    public static MobileElement mobileElement;
+
+    public static boolean elementExist(AndroidDriver driver, int timeLimitInSeconds, String targetResourceId){
+        boolean isElementPresent;
+        try{
+            mobileElement =  (MobileElement) driver.findElementByAndroidUIAutomator("new UiSelector().resourceId(\""+targetResourceId+"\")");
+            WebDriverWait wait = new WebDriverWait(driver, timeLimitInSeconds);
+            wait.until(ExpectedConditions.visibilityOf(mobileElement));
+            isElementPresent = mobileElement.isDisplayed();
+            return isElementPresent;
+        }catch(Exception e){
+            isElementPresent = false;
+            System.out.println(e.getMessage());
+            return isElementPresent;
+        } }
+
+    public static void setCapability(DesiredCapabilities dc) {
+        dc.setCapability(MobileCapabilityType.DEVICE_NAME, "c923d41a");
+        dc.setCapability("platformName", "android");
+        dc.setCapability("appPackage", "com.traveloka.android");
+        dc.setCapability("appActivity", ".appentry.splash.SplashActivity");
+    }
+
+    public static void InitialChooseLanguage(AndroidDriver driver) {
+        while (!(elementExist(driver, TIME_LIMIT, "com.traveloka.android:id/text_continue")));
+        MobileElement el2 = (MobileElement) driver.findElementById("com.traveloka.android:id/text_continue");
+        el2.click();
+
+        while (!(elementExist(driver, TIME_LIMIT, "com.traveloka.android:id/text_skip")));
+        MobileElement el3 = (MobileElement) driver.findElementById("com.traveloka.android:id/text_skip");
+        el3.click();
+
+        while (!(elementExist(driver, TIME_LIMIT, "com.traveloka.android:id/text_skip")));
+        MobileElement el4 = (MobileElement) driver.findElementById("com.traveloka.android:id/text_skip");
+        el4.click();
+
+        while (!(elementExist(driver, TIME_LIMIT, "com.traveloka.android:id/text_view_tooltip_ok")));
+        MobileElement el6 = (MobileElement) driver.findElementById("com.traveloka.android:id/text_view_tooltip_ok");
+        el6.click();
+    }
+
+    public static void AssertOriginCity(AndroidDriver driver, String fromDestination) {
         while (!(elementExist(driver, TIME_LIMIT, "com.traveloka.android:id/image_view_product_icon")));
         MobileElement FlightsButton = (MobileElement) driver.findElement(By.xpath ("//android.widget.ImageView[@bounds='[42,489][174,621]']"));
         FlightsButton.click();
@@ -69,7 +133,9 @@ public class automation {
 
         // Assert From City
         Assert.assertEquals(FromButton.getText(), fromDestination);
+    }
 
+    public static void AssertDestinationCity(AndroidDriver driver, String toDestination) {
         MobileElement ToButton = (MobileElement) driver.findElement(By.xpath ("//android.widget.EditText[@bounds='[84,756][996,876]']"));
         ToButton.click();
 
@@ -81,7 +147,9 @@ public class automation {
 
         // Assert To Destination City
         Assert.assertEquals(ToButton.getText(), toDestination);
-//
+    }
+
+    public static void AssertDate(AndroidDriver driver) {
         MobileElement DepartureDateButton = (MobileElement) driver.findElement(By.xpath ("//android.widget.EditText[@bounds='[84,996][996,1116]']"));
         DepartureDateButton.click();
 
@@ -100,7 +168,9 @@ public class automation {
         while (!(elementExist(driver, TIME_LIMIT, "com.traveloka.android:id/widgetView")));
         // Assert Chosen Date
         Assert.assertTrue(DepartureDateButton.getText().contains(choosenDate));
+    }
 
+    public static void AssertPassenger(AndroidDriver driver) {
         while (!(elementExist(driver, TIME_LIMIT, "com.traveloka.android:id/widgetView")));
         MobileElement Passengers = (MobileElement) driver.findElement(By.xpath ("//android.widget.EditText[@bounds='[84,1237][516,1357]']"));
         Passengers.click();
@@ -112,7 +182,9 @@ public class automation {
 
         MobileElement SelectButton = (MobileElement) driver.findElement(By.xpath ("//android.widget.Button[@text='Select']"));
         SelectButton.click();
+    }
 
+    public static void AssertFlightClass(AndroidDriver driver) {
         while (!(elementExist(driver, TIME_LIMIT, "com.traveloka.android:id/widgetView")));
         MobileElement SeatClass = (MobileElement) driver.findElement(By.xpath ("//android.widget.EditText[@text='Economy']"));
         SeatClass.click();
@@ -123,11 +195,12 @@ public class automation {
 
         MobileElement DoneButton = (MobileElement) driver.findElement(By.xpath ("//android.widget.Button[@text='DONE']"));
         DoneButton.click();
+    }
 
+    public static void AssertChooseFlightSchedule(AndroidDriver driver) {
         while (!(elementExist(driver, TIME_LIMIT, "com.traveloka.android:id/btn_search")));
         MobileElement SearchButton = (MobileElement) driver.findElement(By.xpath ("//android.widget.Button[@text='Search']"));
         SearchButton.click();
-
 
         while (!(elementExist(driver, TIME_LIMIT, "com.traveloka.android:id/recycler_view_content")));
         List<AndroidElement> elementFlight = driver.findElements(By.xpath ("//androidx.recyclerview.widget.RecyclerView[@bounds='[0,316][1080,1800]']/*"));
@@ -139,7 +212,9 @@ public class automation {
         };
         MobileElement selectButton = (MobileElement) driver.findElement(By.xpath ("//android.widget.Button[@text='Select']"));
         selectButton.click();
+    }
 
+    public static void AssertContactDetails(AndroidDriver driver, String NameContactDetails, String MobileNumberContactDetails, String EmailContactDetails) {
         while (!(elementExist(driver, TIME_LIMIT, "com.traveloka.android:id/layout_empty")));
         MobileElement FillInContactDetails = (MobileElement) driver.findElement(By.xpath ("//android.widget.RelativeLayout[@content-desc='trip_booking_view_contactemptycontainer']"));
         FillInContactDetails.click();
@@ -159,7 +234,9 @@ public class automation {
         while (!(elementExist(driver, TIME_LIMIT, "com.traveloka.android:id/button_save")));
         MobileElement Save = (MobileElement) driver.findElementById("com.traveloka.android:id/button_save");
         Save.click();
+    }
 
+    public static void AssertAdult(AndroidDriver driver, String AdultName) {
         swipe(driver, 542, 542, 1458, 400);
 
         while (!(elementExist(driver, TIME_LIMIT, "com.traveloka.android:id/layout_empty")));
@@ -180,7 +257,9 @@ public class automation {
         while (!(elementExist(driver, TIME_LIMIT, "com.traveloka.android:id/button_save")));
         MobileElement SaveAdult = (MobileElement) driver.findElementById("com.traveloka.android:id/button_save");
         SaveAdult.click();
+    }
 
+    public static void AssertChild(AndroidDriver driver, String ChildName, String ChildDate, String ChildMonth, String ChildYear) {
         while (!(elementExist(driver, TIME_LIMIT, "com.traveloka.android:id/layout_empty")));
         MobileElement childDetails = (MobileElement) driver.findElement(By.xpath ("//android.widget.RelativeLayout[@bounds='[24,1227][1056,1395]']"));
         childDetails.click();
@@ -197,7 +276,6 @@ public class automation {
         FullNameChild.sendKeys(ChildName);
 
         MobileElement DateBirthChild = (MobileElement) driver.findElement(By.xpath ("//android.widget.EditText[@bounds='[48,852][1032,1050]']"));
-//        DateBirthChild.sendKeys(ChildBirth);
         DateBirthChild.click();
 
         while (!(elementExist(driver, TIME_LIMIT, "android:id/pickers")));
@@ -259,7 +337,9 @@ public class automation {
         while (!(elementExist(driver, TIME_LIMIT, "com.traveloka.android:id/button_save")));
         MobileElement SaveChild = (MobileElement) driver.findElementById("com.traveloka.android:id/button_save");
         SaveChild.click();
+    }
 
+    public static void AssertInfant(AndroidDriver driver, String InfantName, String InfantDate, String InfantMonth, String InfantYear) {
         while (!(elementExist(driver, TIME_LIMIT, "com.traveloka.android:id/layout_empty")));
         MobileElement infantDetails = (MobileElement) driver.findElementById("com.traveloka.android:id/layout_empty");
         infantDetails.click();
@@ -338,7 +418,9 @@ public class automation {
         SaveInfant.click();
 
         swipe(driver, 542, 542, 1458, 400);
+    }
 
+    public static void AssertFlightDetail(AndroidDriver driver) {
         while (!(elementExist(driver, TIME_LIMIT, "com.traveloka.android:id/text_view_see_below_view")));
         MobileElement CONTINUE = (MobileElement) driver.findElementById("com.traveloka.android:id/text_view_see_below_view");
         CONTINUE.click();
@@ -357,56 +439,6 @@ public class automation {
         MobileElement SelectPaymentMethod = (MobileElement) driver.findElementById("com.traveloka.android:id/text_view_toolbar_title");
         SelectPaymentMethod.getText();
         System.out.println("THE END" + " " + SelectPaymentMethod.getText());
-    }
-
-    public static void swipe(AndroidDriver driver ,int startX, int endX, int startY, int endY) {
-        try {
-            new TouchAction(driver).press(PointOption.point(startX, startY)).waitAction(WaitOptions.waitOptions(Duration.ofSeconds(1)))
-                    .moveTo(PointOption.point(endX, endY)).release().perform();
-        } catch (Exception e) {
-            System.out.println("unable to swipe");
-        }
-    }
-
-    public static MobileElement mobileElement;
-
-    public static boolean elementExist(AndroidDriver driver, int timeLimitInSeconds, String targetResourceId){
-        boolean isElementPresent;
-        try{
-            mobileElement =  (MobileElement) driver.findElementByAndroidUIAutomator("new UiSelector().resourceId(\""+targetResourceId+"\")");
-            WebDriverWait wait = new WebDriverWait(driver, timeLimitInSeconds);
-            wait.until(ExpectedConditions.visibilityOf(mobileElement));
-            isElementPresent = mobileElement.isDisplayed();
-            return isElementPresent;
-        }catch(Exception e){
-            isElementPresent = false;
-            System.out.println(e.getMessage());
-            return isElementPresent;
-        } }
-
-    public static void setCapability(DesiredCapabilities dc) {
-        dc.setCapability(MobileCapabilityType.DEVICE_NAME, "c923d41a");
-        dc.setCapability("platformName", "android");
-        dc.setCapability("appPackage", "com.traveloka.android");
-        dc.setCapability("appActivity", ".appentry.splash.SplashActivity");
-    }
-
-    public static void InitialChooseLanguage(AndroidDriver driver) {
-        while (!(elementExist(driver, TIME_LIMIT, "com.traveloka.android:id/text_continue")));
-        MobileElement el2 = (MobileElement) driver.findElementById("com.traveloka.android:id/text_continue");
-        el2.click();
-
-        while (!(elementExist(driver, TIME_LIMIT, "com.traveloka.android:id/text_skip")));
-        MobileElement el3 = (MobileElement) driver.findElementById("com.traveloka.android:id/text_skip");
-        el3.click();
-
-        while (!(elementExist(driver, TIME_LIMIT, "com.traveloka.android:id/text_skip")));
-        MobileElement el4 = (MobileElement) driver.findElementById("com.traveloka.android:id/text_skip");
-        el4.click();
-
-        while (!(elementExist(driver, TIME_LIMIT, "com.traveloka.android:id/text_view_tooltip_ok")));
-        MobileElement el6 = (MobileElement) driver.findElementById("com.traveloka.android:id/text_view_tooltip_ok");
-        el6.click();
     }
 
     public static int getMonthInNumber(String month) {
